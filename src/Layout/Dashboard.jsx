38bb824a2570a4dc003f49202c2b37c7589cf28c
roksana35 <roksana7235@gmail.com>
2024-06-06 00:30:56 +0600

    add user role base navigation of dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -5,16 +5,18 @@ import { MdAddTask } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import useAuth from "../hooks/useAuth";
 
 
 
 const Dashboard = () => {
+    const {user}=useAuth()
     const axiosSecure = useAxiosSecure();
-    const { data: users = [], refetch } = useQuery({
-        queryKey: ['users'],
+    const { data: isRole = [], refetch } = useQuery({
+        queryKey: [user?.email,'isRole'],
         queryFn: async () => {
             try {
-                const res = await axiosSecure.get('/users');
+                const res = await axiosSecure.get(`/users/${user.email}`);
                 console.log('Users data:', res.data);  // Debug statement
                 return res.data;
             } catch (error) {
@@ -29,97 +31,89 @@ const Dashboard = () => {
             {/* dashboard side bar */}
             <div className="w-64 min-h-screen bg-orange-400">
                 <ul className="menu p-4">
-                {users.map(user => (
-                        <div key={user._id}>
-                            {user.role === 'worker' && (
-                                <>
-                                    <li>
-                                        <NavLink to="/dashboard/Home">
-                                            <FaHome></FaHome>
-                                            Home
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/tasklist">
-                                            <FaList />
-                                            TaskList
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/submission">
-                                            <FaList></FaList>
-                                            My Submissions
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/withdrow">
-                                            <BiMoneyWithdraw />
-                                            withdrawals 
-                                        </NavLink>
-                                    </li>
-                                </>
-                            )}
+                
+                   {
+                    isRole.admin?(
+                        <><li>
+                        <NavLink to="/dashboard/adminhome">
+                            <FaHome></FaHome>
+                           Admin  Home</NavLink>
+                    </li>
+                    <li>
+                                    <NavLink to="/dashboard/manageuser">
+                                        <FaUsers></FaUsers>
+                                        Manage Users</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/managetask">
+                                    <FaTasks />
+                                        Manage Task
+</NavLink>
+                                </li>
 
-                            {user.role === 'taskcreator' && (
-                                <>
-                                    <li>
-                                        <NavLink to="/dashboard/Home">
-                                            <FaHome></FaHome>
-                                             Home
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/addtask">
-                                            <BiTask />
-                                            Add new Tasks
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/mytask">
-                                            <MdAddTask />
-                                            My Task’s
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/coin">
-                                            <BiSolidPurchaseTag />
-                                            Purchase Coin
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/paymentHistory">
-                                            <LuHistory />
-                                            Payment history
-                                        </NavLink>
-                                    </li>
-                                </>
-                            )}
-
-                            {user.role === 'admin' && (
-                                <>
-                                    <li>
-                                        <NavLink to="/dashboard/Home">
-                                            <FaHome></FaHome>
-                                            Home
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/manageuser">
-                                            <FaUsers></FaUsers>
-                                            Manage Users
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/dashboard/managetask">
-                                            <FaTasks />
-                                            Manage Task
-                                        </NavLink>
-                                    </li>
-                                </>
-                            )}
-                        </div>
-                    ))}
-                    
+                    </>
+                    ): isRole.taskcreator ? (
+                        <>
+                            <li>
+                                <NavLink to="/dashboard/taskhome">
+                                    <FaHome />
+                                   TakCreator Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/addtask">
+                                    <BiTask />
+                                    Add New Tasks
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/mytask">
+                                    <MdAddTask />
+                                    My Tasks
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/coin">
+                                    <BiMoneyWithdraw />
+                                    Purchase Coin
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/paymentHistory">
+                                    <LuHistory />
+                                    Payment History
+                                </NavLink>
+                            </li>
+                        </>
+                    ) :isRole.worker ? (
+                        <>
+                            <li>
+                                <NavLink to="/dashboard/workerhome">
+                                    <FaHome />
+                                   Worker Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/tasklist">
+                                    <FaList />
+                                    Task List
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/submission">
+                                    <FaList />
+                                  My Sumission
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/withdraw">
+                                <BiMoneyWithdraw />
+                                    withdrawals 
+                                </NavLink>
+                            </li>
+                        </>
+                    ) : null
+                   }
                         
                             {/* <li>
                                 <NavLink to="/dashboard/Home">
